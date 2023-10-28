615fb33142892c5818b87027ff2c397ce82bb1b7
MSM <qw12345611@naver.com>
2023-10-28 20:28:24 +0900

    초기 설정 완료

--- src/main/java/com/example/soloblog/controller/UserController.java
+++ src/main/java/com/example/soloblog/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.soloblog.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
